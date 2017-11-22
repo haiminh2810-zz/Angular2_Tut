e2e00638d58a50dd77fd121c13c18440d0388888
Minh Nguyen <minhhainguyen@kms-technology.com>
2017-11-22 14:44:54 +0700

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,9 @@ import {TestComponent} from './test.component'
   selector: 'my-app',
   template: `
   <h1>Chao {{name}}</h1>
-  <input type="text" #Name (keyup)="4232"/><br>
-  <button (click)="changeName(Name.value)">Change Name</button>
-  <p>Total agree: {{agree}}</p>
-  <p>Total disagree: {{disagree}}</p>
-  <test *ngFor = "let name of names" [name]= "name" (onVote)=vote($event)></test>
+  
   `,
 })
 export class AppComponent  { 
-  @ViewChildren(TestComponent) 
-  testComponents:Array<TestComponent>;
-  name = 'Angular'; 
-  agree:number=0;
-  disagree:number=0;
-  names=['A','B','C','D']
-  vote(agree:boolean){
-    agree?this.agree++:this.disagree++;
-  }
-  changeName(string:string){
-    console.log(this.testComponents._results[1]);
-  }
+ 
 }
--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -3,20 +3,9 @@ import { Component,Input,Output,EventEmitter } from '@angular/core';
 @Component({
   selector: 'test',
   template: `
-  <h1>Hello {{name}}</h1>  
-  <button  (click)="vote(true)">Agree</button>
-  <button [disabled]="voted" (click)="vote(false)">Disagree</button>
+  
   `,  
 })
 export class TestComponent  { 
-  @Input() name:string;
-  @Output() onVote = new EventEmitter<boolean>();
-  voted=false;
-  vote(agree:boolean){
-    this.voted=true;
-    this.onVote.emit(agree);
-  }
-  setName(string:string){
-    this.name=string;
-  }
+  
 }
